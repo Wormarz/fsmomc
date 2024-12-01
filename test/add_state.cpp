05a2fe082e8a272d02309246ac4440d2a47dad12
wormarz <wormarz@outlook.com>
2024-12-01 22:07:37 +0800

    Enhance state tests, refactor worker1 with STAT macros

--- test/add_state.cpp
+++ test/add_state.cpp
@@ -5,12 +5,14 @@
 #include "catch.hpp"
 
 static bool init_flag;
-static struct working_state* worker1(struct working_state *self)
+static DECLARE_STAT_ACTIONS(worker1)
 {
-    return self;
+    STAT_ACT_START;
+
+    STAT_ACT_END;
 }
 
-static void init1(struct working_state* self)
+static void init1(struct working_state *self)
 {
     init_flag = true;
 }
@@ -18,77 +20,90 @@ static void init1(struct working_state* self)
 TEST_CASE("adding new states")
 {
     ALLOC_SM_INS(buf, 10);
-    REQUIRE(init_state_machine((struct state_machine *)buf, sizeof(buf), 10) == 0);
+    REQUIRE(init_state_machine((struct state_machine *)buf, sizeof(buf), 10) ==
+            0);
     struct state_machine *sm = (struct state_machine *)buf;
 
-    SECTION("Test 1 - Add new state") {
+    SECTION("Test 1 - Add new state")
+    {
         REQUIRE(add_state(sm, "state1", worker1, NULL) == 0);
     }
 
-    SECTION("Test 2 - Init function is asigned") {
+    SECTION("Test 2 - Init function is asigned")
+    {
         init_flag = false;
         REQUIRE(add_state(sm, "state1", worker1, init1) == 0);
         REQUIRE(init_flag == true);
     }
 
-    SECTION("Test 3 - State machine full") {
+    SECTION("Test 3 - State machine full")
+    {
         for (int i = 0; i < sm->stat_nums; ++i) {
             REQUIRE(add_state(sm, "state1", worker1, NULL) == 0);
         }
         REQUIRE(add_state(sm, "state1", worker1, NULL) != 0);
     }
 
-    SECTION("Test 4 - Empty state name") {
+    SECTION("Test 4 - Empty state name")
+    {
         REQUIRE(add_state(sm, NULL, worker1, NULL) != 0);
     }
 
-    SECTION("Test 5 - State machine is NULL") {
+    SECTION("Test 5 - State machine is NULL")
+    {
         REQUIRE(add_state(NULL, "state1", worker1, NULL) != 0);
     }
-
 }
 
-TEST_CASE("adding sub-states") {
+TEST_CASE("adding sub-states")
+{
     ALLOC_SM_INS(buf, 10);
-    REQUIRE(init_state_machine((struct state_machine *)buf, sizeof(buf), 10) == 0);
+    REQUIRE(init_state_machine((struct state_machine *)buf, sizeof(buf), 10) ==
+            0);
     struct state_machine *sm = (struct state_machine *)buf;
 
-    SECTION("Test 1 - Normal case") {
+    SECTION("Test 1 - Normal case")
+    {
         REQUIRE(add_state(sm, "abs-st", NULL, NULL) == 0);
         REQUIRE(add_substate(sm, "abs-st", "state1", worker1, NULL) == 0);
     }
 
-    SECTION("Test 2 - Init function is asigned") {
+    SECTION("Test 2 - Init function is asigned")
+    {
         init_flag = false;
         REQUIRE(add_state(sm, "abs-st", NULL, NULL) == 0);
         REQUIRE(add_substate(sm, "abs-st", "state1", worker1, init1) == 0);
         REQUIRE(init_flag == true);
     }
 
-    SECTION("Test 2 - State does not exist") {
+    SECTION("Test 2 - State does not exist")
+    {
         REQUIRE(add_substate(sm, "abs-st", "state1", worker1, NULL) != 0);
     }
 
-    SECTION("Test 3 - Empty state name") {
+    SECTION("Test 3 - Empty state name")
+    {
         REQUIRE(add_state(sm, "abs-st", NULL, NULL) == 0);
         REQUIRE(add_substate(sm, NULL, "state1", worker1, NULL) != 0);
     }
 
-    SECTION("Test 4 - Empty sub-state name") {
+    SECTION("Test 4 - Empty sub-state name")
+    {
         REQUIRE(add_state(sm, "abs-st", NULL, NULL) == 0);
         REQUIRE(add_substate(sm, "abs-st", NULL, worker1, NULL) != 0);
     }
 
-    SECTION("Test 5 - State machine full") {
+    SECTION("Test 5 - State machine full")
+    {
         for (int i = 0; i < sm->stat_nums; ++i) {
             REQUIRE(add_state(sm, "state1", worker1, NULL) == 0);
         }
         REQUIRE(add_substate(sm, "abs-st", "state1", worker1, NULL) != 0);
     }
 
-    SECTION("Test 6 - State machine is NULL") {
+    SECTION("Test 6 - State machine is NULL")
+    {
         REQUIRE(add_state(sm, "abs-st", NULL, NULL) == 0);
         REQUIRE(add_substate(NULL, "abs-st", "state1", worker1, NULL) != 0);
     }
 }
-
